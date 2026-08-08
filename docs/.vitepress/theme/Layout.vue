a7627e3736f23377a3929fdd79ffd324fc77967c
loonghao <hal.long@outlook.com>
2026-08-09 00:13:57 +0800

    feat: add gsap homepage motion

---
+++ docs/.vitepress/theme/Layout.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import DefaultTheme from 'vitepress/theme'
+import HomeMotion from './components/HomeMotion.vue'
+
+const { Layout } = DefaultTheme
+</script>
+
+<template>
+  <Layout />
+  <HomeMotion />
+</template>
